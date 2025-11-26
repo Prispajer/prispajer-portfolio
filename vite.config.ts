63d7b5895188ab8209bf50ba576fa90921626cad
Adrian Kozieł <koziel.adrian98@gmail.com>
2025-11-26 22:19:32 +0100

    chore(setup): Removed unused assets, updated index.css colors, updated favicon, upgraded Tailwind from v3 to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest/config" />
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
