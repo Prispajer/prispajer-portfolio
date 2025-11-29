ca6620e4d98208823282812a38d7b45f3c02c3d6
Adrian Kozieł <koziel.adrian98@gmail.com>
2025-11-29 02:53:31 +0100

    chore(data): Created data file for site info

---
+++ src/data/data.ts
@@ -0,0 +1,15 @@
+﻿import {Briefcase, Github, Home, Linkedin, Mail, MessageCircle, User} from "lucide-react";
+
+export const navigationItems = [
+    {id: "hero", name: "Hero", icon: Home},
+    {id: "about", name: "About", icon: User},
+    {id: "projects", name: "Projects", icon: Briefcase},
+    {id: "contact", name: "Contact", icon: Mail},
+]
+
+export const socialItems = [
+    {icon: Github, link: "https://github.com/prispajer"},
+    {icon: Linkedin, link: "https://www.linkedin.com/in/adrian-kozie%C5%82/"},
+    {icon: MessageCircle, link: "https://www.facebook.com/adikoxi/"},
+    {icon: Mail, link: "mailto:koziel.adrian98@gmail.com"},
+]
